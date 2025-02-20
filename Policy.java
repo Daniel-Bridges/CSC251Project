de6f18b7df04ae2048056fdc05e16cb6e87492a3
Hayes <dbridge9@email.cpcc.edu>
2025-02-20 16:13:30 -0500

    Update Policy.java
    Moved fields and methods to the policy holder

--- Policy.java
+++ Policy.java
@@ -2,14 +2,7 @@ class Policy {
    
    private int policy_number;
    private String provider_name;
-   private String policy_holder_first_name;
-   private String policy_holder_last_name;
-   private int policy_holder_age;
-   private String policy_holder_smoking_status; // Either "smoker" or "non-smoker"
-   private double policy_holder_height; // in inches
-   private double policy_holder_weight; // in pounds
-   
-   
+     
    /**
      * No args constructor
      */
@@ -17,12 +10,6 @@ class Policy {
    {
       this.policy_number = 0000;
       this.provider_name = "none";
-      this.policy_holder_first_name = "John";
-      this.policy_holder_last_name = "Doe";
-      this.policy_holder_age = 00;
-      this.policy_holder_smoking_status = "none";
-      this.policy_holder_height = 00.00;
-      this.policy_holder_weight = 00.00;
    }
    
    /**
@@ -30,25 +17,11 @@ class Policy {
      *
      * @param policy_number   Unique number for each policy
      * @param provider_name   The policy provider's name
-     * @param policy_holder_first_name    Policy holders first name
-     * @param policy_holder_last_name  Policy holders last name
-     * @param policy_holder_age  Policy holders age
-     * @param policy_holder_smoking_status   Policy holder's smoking status 
-     * @param policy_holder_height  Policy holder's height
-     * @param policy_holder_weight  Policy holder's weight
      */
-   public Policy(int policy_number, String provider_name, String policy_holder_first_name, 
-   String policy_holder_last_name, int policy_holder_age, String policy_holder_smoking_status, 
-   double policy_holder_height, double policy_holder_weight) 
+   public Policy(int policy_number, String provider_name) 
    { 
       this.policy_number = policy_number;
       this.provider_name = provider_name;
-      this.policy_holder_first_name = policy_holder_first_name;
-      this.policy_holder_last_name = policy_holder_last_name;
-      this.policy_holder_age = policy_holder_age;
-      this.policy_holder_smoking_status = policy_holder_smoking_status;
-      this.policy_holder_height = policy_holder_height;
-      this.policy_holder_weight = policy_holder_weight;
    }
    
    // Setters
@@ -73,66 +46,7 @@ class Policy {
       this.provider_name = provider_name;
    }
    
-   /**
-     * Sets the policy holder's first name.
-     *
-     * @param policy_holder_first_name the policy holders first name
-     */
-   public void set_policy_holder_first_name(String policy_holder_first_name)
-   {
-      this.policy_holder_first_name = policy_holder_first_name;
-   }
-   
-   /**
-     * Sets the policy holder's last name.
-     *
-     * @param policy_holder_last_name the policy holders last name
-     */
-   public void set_policy_holder_last_name(String policy_holder_last_name)
-   {
-      this.policy_holder_last_name = policy_holder_last_name;
-   }
-   
-   /**
-     * Sets the policy holder's age.
-     *
-     * @param policy_holder_age the policy holders age name
-     */
-   public void set_policy_holder_age(int policy_holder_age)
-   {
-      this.policy_holder_age = policy_holder_age;
-   }
-   
-   /**
-     * Sets the policy holder's smoking status.
-     *
-     * @param policy_holder_smoking_status the policy holders smoking status
-     */
-   public void set_policy_holder_smoking_status(String policy_holder_smoking_status)
-   {
-      this.policy_holder_smoking_status = policy_holder_smoking_status;
-   }
-   
-   /**
-     * Sets the policy holder's height.
-     *
-     * @param policy_holder_height the policy holders height
-     */
-   public void set_policy_holder_height(double policy_holder_height)
-   {
-      this.policy_holder_height = policy_holder_height;
-   }
-   
-   /**
-     * Sets the policy holder's weight.
-     *
-     * @param policy_holder_weight the policy holders weight
-     */
-   public void set_policy_holder_weight(double policy_holder_weight)
-   {
-      this.policy_holder_weight = policy_holder_weight;
-   }
-   
+      
    // Getters
    
    
@@ -156,78 +70,8 @@ class Policy {
       return this.provider_name;
    }
    
-   /**
-     * Gets the policy holders first name.
-     *
-     * @return the policy holders first name as a String
-     */
-   public String get_policy_holder_first_name()
-   {
-      return this.policy_holder_first_name;
-   }
-   
-   /**
-     * Gets the policy holders last name.
-     *
-     * @return the policy holders last name as a String
-     */
-   public String get_policy_holder_last_name()
-   {
-      return this.policy_holder_last_name;
-   }
-   
-   /**
-     * Gets the policy holders age.
-     *
-     * @return the policy holders age as an in
-     */
-   public int get_policy_holder_age()
-   {
-      return this.policy_holder_age;
-   }
-   
-   /**
-     * Gets the policy holders smoking status.
-     *
-     * @return the policy holders smoking status as a String
-     */
-   public String get_policy_holder_smoking_status()
-   {
-      return this.policy_holder_smoking_status;
-   }
-   
-   /**
-     * Gets the policy holders height.
-     *
-     * @return the policy holders height as a double
-     */
-   public double get_policy_holder_height()
-   {
-      return this.policy_holder_height;
-   }
-   
-   /**
-     * Gets the policy holders height.
-     *
-     * @return the policy holders weight as a double
-     */
-   public double get_policy_holder_weight()
-   {
-      return this.policy_holder_weight;
-   }
-   
+      
    // Other Functions
-   
-   /**
-     * Calculates the BMI index
-     * Uses the policy holder's weight and height
-     *
-     * @return the calculated BMI
-     */
-   public double calculate_body_mass_index()
-   {
-      return (policy_holder_weight * 703) / (policy_holder_height * policy_holder_height);
-   }
    
    /**
      * Calculates the insurance price
